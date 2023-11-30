e9b5190b149388e37d9a6fa060ff07503657e79d
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-11-29 22:07:36 -0300

    created styles for dashboard activity

--- app/src/main/java/com/example/onehabits/MainActivity.java
+++ app/src/main/java/com/example/onehabits/MainActivity.java
@@ -36,10 +36,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
-
-
-
-
 }
--- app/src/main/java/com/example/onehabits/dashboard.java
+++ app/src/main/java/com/example/onehabits/dashboard.java
@@ -1,8 +1,12 @@
 package com.example.onehabits;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class dashboard extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        LinearLayout cardContainer = findViewById(R.id.card_dou);
+
+        // Infla la vista de la tarjeta desde el modelo XML
+        CardView cardView = (CardView) LayoutInflater.from(this).inflate(R.layout.card, cardContainer, false);
+
+        // Configura elementos dentro de la tarjeta
+        TextView textTitle = cardView.findViewById(R.id.title);
+        textTitle.setText("Nuevo Título");
+
+        // Agrega la tarjeta al contenedor
+        cardContainer.addView(cardView);
     }
 }
